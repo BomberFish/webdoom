f7ea6835fe3e9a7e9ffa8e48e8d6e2e96a09198f
BomberFish <BomberFish@users.noreply.github.com>
2023-01-03 23:35:30 +0000

    deploy: 47e5fd5ea0895412a9c0149c9e09e635ba31ffb3

---
+++ rust/target/wasm32-unknown-unknown/release/deps/rust-ae86a7f496ed04a4.d
@@ -0,0 +1,5 @@
+/home/runner/work/webdoom/webdoom/rust/target/wasm32-unknown-unknown/release/deps/rust-ae86a7f496ed04a4.wasm: src/main.rs
+
+/home/runner/work/webdoom/webdoom/rust/target/wasm32-unknown-unknown/release/deps/rust-ae86a7f496ed04a4.d: src/main.rs
+
+src/main.rs:
---
+++ rust/target/wasm32-unknown-unknown/release/rust.d
@@ -0,0 +1,1 @@
+/home/runner/work/webdoom/webdoom/rust/target/wasm32-unknown-unknown/release/rust.wasm: /home/runner/work/webdoom/webdoom/rust/src/main.rs
